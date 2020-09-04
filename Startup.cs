ff3844a7fd269b0adaac273d09fa2d4906335365
rhafen <rhafen@varis1.com>
2020-09-04 08:32:47 -0700

    added data seeding

--- Startup.cs
+++ Startup.cs
@@ -10,11 +10,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Advantage.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Advantage.API
 {
     public class Startup
     {
+        private string _connectionString = null;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,11 +28,19 @@ namespace Advantage.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            _connectionString = Configuration["secretConnectionString"];
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddEntityFrameworkNpgsql()
+            .AddDbContext<ApiContext>(
+                opt => opt.UseNpgsql(_connectionString));
+
+            services.AddTransient<DataSeed>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, DataSeed seed)
         {
             if (env.IsDevelopment())
             {
@@ -39,6 +50,8 @@ namespace Advantage.API
             {
                 app.UseHsts();
             }
+
+            seed.SeedData(20, 1000);
 
             app.UseHttpsRedirection();
             app.UseMvc();
